3e8991e8d6e3f8233395e02c8dbc6cead68b8b0d
Spurthy Mutturaj <spurthymu99@gmail.com>
2022-07-12 06:15:23 +0000

    Sync LeetCode submission - Linked List Cycle (python3)

--- my-folder/problems/linked_list_cycle/solution.py
+++ my-folder/problems/linked_list_cycle/solution.py
@@ -6,19 +6,23 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        li=[]
-        addr=head
-
-        while True:
-            if addr == None:
-                return False
-                break
-            if addr not in li:
-                li.append(addr)
-                addr=addr.next
-            else:
+        
+        fast=head
+        slow=head
+        while fast is not None and fast.next is not None:
+            fast=fast.next.next
+            slow=slow.next
+            if slow==fast:
                 return True
-        
-    
-        
-    
+        return False
+        '''
+        d=set()
+        ptr1=head
+
+        while ptr1:
+            if ptr1 in d:
+                return True
+            
+            d.add(ptr1)
+            ptr1=ptr1.next
+        return False'''
