d6efdaa42a9648e20a2265583dea6efd96b87b5f
Spurthy Mutturaj <spurthymu99@gmail.com>
2022-11-10 19:43:18 +0000

    Sync LeetCode submission - Roman to Integer (python3)

--- my-folder/problems/roman_to_integer/solution.py
+++ my-folder/problems/roman_to_integer/solution.py
@@ -1,24 +1,22 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        sum=0
-        d={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
-        s=s.replace('IV','IIII')
-        s=s.replace('IX','VIIII')
-        s = s.replace("XL", "XXXX")
-        s=s.replace("XC", "LXXXX")
-        s =s.replace("CD", "CCCC")
-        s=s.replace("CM", "DCCCC")
-            
-        
-        for x in s:
-            
-           
-         
+        d={
+            "I":1,
+            "V":5,
+            "X":10,
+            "L":50,
+            "C":100,
+            "D":500,
+            "M":1000
+        }
+        total=0
+        for i in range(len(s)-1):
+            if d[s[i]]<d[s[i+1]]:
+                total=total-d[s[i]]
+ 
+            else:
+                total+=d[s[i]]
+        return total+d[s[-1]]
                 
-            sum=sum+d[x]
             
-    
-        return(sum)
-    
-    
-        
+            
