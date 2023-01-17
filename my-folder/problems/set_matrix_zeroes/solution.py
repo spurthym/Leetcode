3c33848496cc8d87e07319488036ea28bc92183c
Spurthy Mutturaj <spurthymu99@gmail.com>
2023-01-17 14:34:12 +0000

    Sync LeetCode submission - Set Matrix Zeroes (python3)

--- my-folder/problems/set_matrix_zeroes/solution.py
+++ my-folder/problems/set_matrix_zeroes/solution.py
@@ -3,10 +3,9 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        
         rows=len(matrix)
         cols=len(matrix[0])
-        zerorow=False
+        rowz=False
         for r in range(rows):
             for c in range(cols):
                 if matrix[r][c]==0:
@@ -14,19 +13,16 @@ class Solution:
                     if r>0:
                         matrix[r][0]=0
                     else:
-                        zerorow=True
+                        rowz=True
+        
         for r in range(1,rows):
             for c in range(1,cols):
-                if matrix[r][0]==0 or matrix[0][c]==0:
+                if matrix[0][c]==0 or matrix[r][0]==0:
                     matrix[r][c]=0
         
         if matrix[0][0]==0:
             for r in range(rows):
                 matrix[r][0]=0
-            
-        if zerorow==True:
+        if rowz:
             for c in range(cols):
                 matrix[0][c]=0
-
-                
-                
