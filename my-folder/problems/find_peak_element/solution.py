513210418c652425bafe3bc87266bbdd54ec8f11
Spurthy Mutturaj <spurthymu99@gmail.com>
2023-02-01 06:16:02 +0000

    Sync LeetCode submission - Find Peak Element (python3)

--- my-folder/problems/find_peak_element/solution.py
+++ my-folder/problems/find_peak_element/solution.py
@@ -4,10 +4,12 @@ class Solution:
         l=0
         r=len(nums)-1
         while l<r:
+            print(l,r)
             mid=(l+r)//2
+            print(mid)
             if nums[mid]>nums[mid+1]:
                 r=mid
             else :
                 l=mid+1
         
-        return l
+        return r
