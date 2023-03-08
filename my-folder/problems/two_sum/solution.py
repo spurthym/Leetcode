3a57b042d262f6e8b8ac0a8471ba7facba96ddfa
Spurthy Mutturaj <spurthymu99@gmail.com>
2023-03-08 18:48:01 +0000

    Sync LeetCode submission - Two Sum (python3)

--- my-folder/problems/two_sum/solution.py
+++ my-folder/problems/two_sum/solution.py
@@ -1,11 +1,16 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         lookup={}
-        
+
         for i in range(len(nums)):
-            curr_val=nums[i]
-            if curr_val in lookup:
-                return [i,lookup[curr_val]]
-                
-            else:
-                lookup[target-curr_val]=i
+            if nums[i] in lookup:
+                return [i,lookup[nums[i]]]
+            
+            
+            lookup[target-nums[i]]=i
+         
+            
+
+        
+        
+        
