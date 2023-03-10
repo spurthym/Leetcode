0ca271ad405e940a0cdcdee9ac31efc24f9953cd
Spurthy Mutturaj <spurthymu99@gmail.com>
2023-03-10 22:59:27 +0000

    Sync LeetCode submission - Valid Sudoku (python3)

--- my-folder/problems/valid_sudoku/solution.py
+++ my-folder/problems/valid_sudoku/solution.py
@@ -1,17 +1,26 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
+        #rows validation
         rows=defaultdict(set)
         cols=defaultdict(set)
         grid=defaultdict(set)
-        for r in range(len(board)):
-            for c in range(len(board)):
-                if board[r][c]==".":
+
+        for i in range(9):
+            for j in range(9):
+                spos=(i//3*3+j//3)
+                print(spos)
+                if board[i][j]==".":
                     continue
-                if (board[r][c] in rows[r] or 
-                board[r][c] in cols[c]
-                or board[r][c] in grid[r//3,c//3]):
+                elif board[i][j] in rows[i] or board[i][j] in cols[j] or board[i][j] in grid[spos]:
                     return False
-                rows[r].add(board[r][c])
-                cols[c].add(board[r][c])
-                grid[(r//3,c//3)].add(board[r][c])
+                else:
+                    rows[i].add(board[i][j])
+                    cols[j].add(board[i][j])
+                    grid[spos].add(board[i][j])
         return True
+
+
+
+
+
+        
