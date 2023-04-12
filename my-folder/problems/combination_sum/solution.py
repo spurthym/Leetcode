3d4c248211e9f1eec7c2a6cb3c14cbda5ad68253
Spurthy Mutturaj <spurthymu99@gmail.com>
2023-04-12 05:20:20 +0000

    Sync LeetCode submission - Combination Sum (python3)

--- my-folder/problems/combination_sum/solution.py
+++ my-folder/problems/combination_sum/solution.py
@@ -1,18 +1,28 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        res=[]
-        cur=[]
-        def dfs(i,cur,total):
-            if total==target:
-                res.append(cur.copy())
+        temp = []
+        res = []
+        s=0
+        candidates.sort()
+        
+        def backtrack(start,s):
+            if s > target:
                 return
-            if total> target or i>=len(candidates):
+            if s == target:
+                res.append(temp.copy())
                 return
-            
-            cur.append(candidates[i])
-            dfs(i,cur,total+candidates[i])
-            cur.pop()
-            dfs(i+1,cur,total)
-        dfs(0,[],0)
+            for i in range(start, len(candidates)):
+                
+                if s + candidates[i] > target:  # added condition
+                    break
+                temp.append(candidates[i])
+                print(temp)
+                s += candidates[i]
+                backtrack(i,s)
+                temp.pop()
+                s -= candidates[i]
+
+
+                
+        backtrack(0,s)
         return res
-            
