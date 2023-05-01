57fcd4ecbed4cbd1f07a4adca42337239c2b5915
Spurthy Mutturaj <spurthymu99@gmail.com>
2023-05-01 18:15:54 +0000

    Sync LeetCode submission - Rotting Oranges (python3)

--- my-folder/problems/rotting_oranges/solution.py
+++ my-folder/problems/rotting_oranges/solution.py
@@ -1,39 +1,31 @@
-from collections import deque
-from typing import List
-
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
+        
 
-        directions=[[-1,0],[0,-1],[1,0],[0,1]]
         rows=len(grid)
         cols=len(grid[0])
 
+        directions=[[1,0],[-1,0],[0,1],[0,-1],]
+
         q=deque()
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j]==2:
+                    q.append((i,j,0))
         
-        # Add all initially rotten oranges to the queue
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c] == 2:
-                    q.append((r, c, 0))
-
-        # Perform BFS to rot the oranges
-        m = 0
+        m=0
         while q:
-            ro, co, t = q.popleft()
-            m = max(m, t)
+            r,c,t=q.popleft()
+            m=max(t,m)
+            for dr,dc in directions:
+                ro=dr+r
+                co=dc+c
 
-            for dr, dc in directions:
-                r = dr + ro
-                c = dc + co
-
-                if r >= 0 and c >= 0 and r < rows and c < cols and grid[r][c] == 1:
-                    q.append((r, c, t+1))
-                    grid[r][c] = 2
-
-        # Check if there are any fresh oranges left
+                if ro in range(rows) and co in range(cols) and grid[ro][co]==1:
+                    q.append((ro,co,t+1))
+                    grid[ro][co]=2
+        
         for row in grid:
             if 1 in row:
-                return -1 # if there are fresh oranges left, return -1
-
+                return -1
         return m
-        
