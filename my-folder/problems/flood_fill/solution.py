208badaf6bad2b73ac0206d3b74cb3c597c47ffc
Spurthy Mutturaj <spurthymu99@gmail.com>
2023-06-02 08:35:08 +0000

    Sync LeetCode submission - Flood Fill (python3)

--- my-folder/problems/flood_fill/solution.py
+++ my-folder/problems/flood_fill/solution.py
@@ -1,27 +1,39 @@
 class Solution:
-    def floodFill(self, img: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        rows=len(img)
-        cols=len(img[0])
-        directions=[[0,-1],[0,1],[-1,0],[1,0]]
-        visit=set()
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        lrow=len(image)
+        lcol=len(image[0])
 
-        org=img[sr][sc]
-        q=collections.deque()
-        q.append((sr,sc))
-        visit.add((sr,sc))
-        img[sr][sc]=color
+        stack=[]
+        directions=[(+1,0),(-1,0),(0,-1),(0,+1)]
+        visited=set()
+        oc=image[sr][sc]
 
-        while q:
-            ro,co=q.popleft()
+
+
+
+        def dfs(row,col):
+
             for dr,dc in directions:
-                r=ro+dr
-                c=co+dc
+                r=dr+row
+                c=dc+col
 
-                if r in range(rows) and c in range(cols) and (r,c) not in visit and img[r][c]==org:
-                    q.append((r,c))
-                    visit.add((r,c))
-                    img[r][c]=color
-        return img
+                if r in range(lrow) and c in range(lcol) and image[r][c]==oc and (r,c) not in visited:
+                    visited.add((r,c))
+                    stack.append((r,c))
+                    image[r][c]=color
+                    dfs(r,c)
+                    stack.pop()
+        
+        image[sr][sc]=color
+        stack.append((sr,sc))
+        visited.add((sr,sc))
+        dfs(sr,sc)
+        return image
+
+        
+
+
+
 
 
         
