4fee8743328bf7a193610d7ac89305a9b69576e2
Spurthy Mutturaj <spurthymu99@gmail.com>
2023-06-02 09:38:08 +0000

    Sync LeetCode submission - Number of Islands (python3)

--- my-folder/problems/number_of_islands/solution.py
+++ my-folder/problems/number_of_islands/solution.py
@@ -1,36 +1,45 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        
-        rows=len(grid)
-        cols=len(grid[0])
-        visit=set()
-        island=0
-        directions=[[-1,0],[1,0],[0,1],[0,-1]]
 
-        def bfs(r,c):
-            q=collections.deque()
-            visit.add((r,c))
-            q.append((r,c))
+        directions=[(1,0),(-1,0),(0,1),(0,-1)]
+        lrow=len(grid)
+        lcol=len(grid[0])
+        visited=set()
+        count=0
+        stack=[]
+        q=collections.deque()
+        def bfs(row,col):
 
-            while q:
-                ro,co=q.popleft()
+            q.append((row,col))
+
+            while (q):
+                q.popleft()
                 for dr,dc in directions:
-                    r=ro+dr
-                    c=co+dc
-                    if (r in range(rows) and c in range(cols) and 
-                       grid[r][c]=="1" and (r,c) not in visit):
+                    r=row+dr
+                    c=col+dc
+
+                    if r in range(lrow) and c in range(lcol) and grid[r][c]=="1" and (r,c) not in visited:
+                        visited.add((r,c))
                         q.append((r,c))
-                        visit.add((r,c))
-                        
-        
-        
-        
-        
-        
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c]=="1" and (r,c) not in visit:
-                    bfs(r,c)
-                    island+=1
-        return island
+                        bfs(r,c)
+            
+
+
+
+
+
+
+
+        for i in range(lrow):
+            for j in range(lcol):
+                if grid[i][j]=="1" and (i,j) not in visited:
+
+                    count+=1
+                    bfs(i,j)
+                   
+        return count
+
+
+
+
         
