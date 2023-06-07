ad9bce7c60ff6e61a61b33b2007893f5da075dba
Spurthy Mutturaj <spurthymu99@gmail.com>
2023-06-07 16:43:53 +0000

    Sync LeetCode submission - Course Schedule (python3)

--- my-folder/problems/course_schedule/solution.py
+++ my-folder/problems/course_schedule/solution.py
@@ -3,31 +3,28 @@ from collections import defaultdict
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         d = defaultdict(list)
-        for i in range(numCourses):
-            d[i] = []
+
         for j in range(len(prerequisites)):
             d[prerequisites[j][0]].append(prerequisites[j][1])
 
-        visited = set()
-        memo = {}
+        def dfs(node):
+            visited[node] = True
+            stk[node] = True
 
-        def isfinishable(node):
-            if node in visited:
-                return False
-            if node in memo:
-                return memo[node]
-            visited.add(node)
-            for j in d[node]:
-                if not isfinishable(j):
-                    memo[node] = False
-                    visited.remove(node)
+            for each in d[node]:
+                if visited[each] == False:
+                    if dfs(each):
+                        return True
+                elif stk[each] == True:
+                    return True
+            stk[node] = False
+            return False
+
+        visited = [False] * numCourses
+        stk = [False] * numCourses
+
+        for k in range(numCourses):
+            if not visited[k]:
+                if dfs(k):
                     return False
-            visited.remove(node)
-            memo[node] = True
-            return True
-
-        for each in d:
-            if not isfinishable(each):
-                return False
-
         return True
