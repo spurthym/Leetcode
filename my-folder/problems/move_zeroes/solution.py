c58f038e914f819f261255ea39f932e508f621f7
Spurthy Mutturaj <spurthymu99@gmail.com>
2023-07-15 20:34:37 +0000

    Sync LeetCode submission - Move Zeroes (python)

--- my-folder/problems/move_zeroes/solution.py
+++ my-folder/problems/move_zeroes/solution.py
@@ -1,13 +1,17 @@
-class Solution:
-    def moveZeroes(self, nums: List[int]) -> None:
+class Solution(object):
+    def moveZeroes(self, nums):
         """
-        Do not return anything, modify nums in-place instead.
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
         """
+        res=[0 for x in range(len(nums))]
         j=0
         for i in range(len(nums)):
             if nums[i]==0:
                 continue
-            nums[j]=nums[i]
-            j=j+1
+            else:
+                nums[j]=nums[i]
+                j+=1
         for k in range(j,len(nums)):
             nums[k]=0
+        return nums
