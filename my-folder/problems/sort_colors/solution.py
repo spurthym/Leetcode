7f72802ad4366d899623bfec43824c1103c735a9
Spurthy Mutturaj <spurthymu99@gmail.com>
2023-07-16 22:24:28 +0000

    Sync LeetCode submission - Sort Colors (python)

--- my-folder/problems/sort_colors/solution.py
+++ my-folder/problems/sort_colors/solution.py
@@ -1,16 +1,27 @@
-class Solution:
-    def sortColors(self, n: List[int]) -> None:
-        p1,p2,p3=0,0,len(n)-1
-        while p2<=p3:
-            if n[p2]==0:
-                n[p1],n[p2]=n[p2],n[p1]
-                p1+=1
-                p2+=1
-            elif n[p2]==1:
-                p2+=1
-            else:
-                n[p2],n[p3]=n[p3],n[p2]
-                p3-=1
+class Solution(object):
+    def sortColors(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+
+        i=0
+        l=0
+        r=len(nums)-1
+
+        def swap(i,j):
+            temp=nums[i]
+            nums[i]=nums[j]
+            nums[j]=temp
+
+        while i<=r:
+            if nums[i]==0:
+                swap(l,i)
+                l+=1
+            if nums[i]==2:
+                swap(r,i)
+                r-=1
+                i-=1
             
-                
-                
+
+            i+=1
