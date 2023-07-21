9cb926d18acd6eb4e4efaf218aad9fa922b829cc
Spurthy Mutturaj <spurthymu99@gmail.com>
2023-07-21 23:24:42 +0000

    Sync LeetCode submission - 3Sum (python)

--- my-folder/problems/3sum/solution.py
+++ my-folder/problems/3sum/solution.py
@@ -1,29 +1,39 @@
-class Solution:
-    def threeSum(self, arr: List[int]) -> List[List[int]]:
-        arr.sort()
-        res=[]
-        
-        for i,a in enumerate(arr):
-            if i>0 and a==arr[i-1]:
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        Find all unique triplets in the array whose sum equals zero.
+
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        nums.sort()
+
+        res = []
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i - 1]:
                 continue
-            
-            left=i+1
-            right=len(arr)-1
-            
-            while left<right:
-                s=a+arr[left]+arr[right]
-                if s<0:
-                    left+=1
-                elif s>0:
-                    right-=1
+
+            a = i + 1
+            b = len(nums) - 1
+
+            while a < b:
+                s = nums[i] + nums[a] + nums[b]
+
+                if s < 0:
+                    a += 1
+                elif s > 0:
+                    b -= 1
                 else:
-                    res.append([a,arr[left],arr[right]])
-                    left+=1
-                    right-=1
-                    while left<right and arr[left]==arr[left-1]:
-                        left+=1
-                    while left<right and arr[right]==arr[right+1]:
-                        right-=1
+                    res.append([nums[i], nums[a], nums[b]])
+
+                    # Skip duplicates for the second and third elements
+                    while a < b and nums[a] == nums[a + 1]:
+                        a += 1
+                    while a < b and nums[b] == nums[b - 1]:
+                        b -= 1
+
+                    # Move both pointers to explore other potential triplets
+                    a += 1
+                    b -= 1
+
         return res
-                    
-                    
