5adb1a61f71e4443bcf3c3388953cf2b4762bcc9
Spurthy Mutturaj <spurthymu99@gmail.com>
2023-07-28 21:55:09 +0000

    Sync LeetCode submission - Palindrome Linked List (python3)

--- my-folder/problems/palindrome_linked_list/solution.py
+++ my-folder/problems/palindrome_linked_list/solution.py
@@ -5,12 +5,27 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        
-        stack=[]
-        while head:
-            stack.append(head.val)
-            head=head.next
+        fast,slow=head,head
+        stk=[]
+        while fast and fast.next:
+            stk.append(slow.val)
+            slow=slow.next
+            fast=fast.next.next
+        if fast:
+            slow=slow.next
             
-        return stack==stack[::-1]
-            
+
+
+        while slow and stk:
+
+            if slow.val==stk[-1]:
+                stk.pop()
+                slow=slow.next
+            else:
+                return False
         
+        if len(stk)>0:
+            return False
+    
+        return True
+
