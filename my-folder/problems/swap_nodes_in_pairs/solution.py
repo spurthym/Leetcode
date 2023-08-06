cd84e73f6a343e4ce3f8448623a42f9fa4a9dffb
Spurthy Mutturaj <spurthymu99@gmail.com>
2023-08-06 23:27:55 +0000

    Sync LeetCode submission - Swap Nodes in Pairs (python3)

--- my-folder/problems/swap_nodes_in_pairs/solution.py
+++ my-folder/problems/swap_nodes_in_pairs/solution.py
@@ -5,19 +5,12 @@
 #         self.next = next
 class Solution:
     def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        dummy=ListNode(0)
-        dummy.next=head
-        curr=head
-        prev=dummy
-        while curr and curr.next:
-            t1=curr.next
-            t2=curr.next.next
-            
-            t1.next=curr
-            curr.next=t2
-            prev.next=t1
-            
-            prev=curr
-            curr=t2
-        return dummy.next
         
+        curr=head
+
+        while curr and curr.next:
+            temp=curr.val
+            curr.val=curr.next.val
+            curr.next.val=temp
+            curr=curr.next.next
+        return head
