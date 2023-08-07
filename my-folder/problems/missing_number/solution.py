f88e67162ad3d5bd55858c298f012aa98226e8e3
Spurthy Mutturaj <spurthymu99@gmail.com>
2023-08-07 20:50:51 +0000

    Sync LeetCode submission - Missing Number (python3)

--- my-folder/problems/missing_number/solution.py
+++ my-folder/problems/missing_number/solution.py
@@ -1,8 +1,20 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        n=len(nums)
+
         i=0
-        while i in nums:
-            i+=1
-        return i
+
+        while i<len(nums):
+            j=nums[i]
+
+            if nums[i]< len(nums) and nums[i]!=nums[j]:
+
+                temp=nums[i]
+                nums[i]=nums[j]
+                nums[j]=temp
+            else:
+                i+=1
         
+        for i in range(len(nums)):
+            if nums[i] != i :
+                return i
+        return len(nums)
