30082f06b0fa6f0db896bb895b3db7d34847efe7
Spurthy Mutturaj <spurthymu99@gmail.com>
2023-08-17 23:27:42 +0000

    Sync LeetCode submission - Populating Next Right Pointers in Each Node (python3)

--- my-folder/problems/populating_next_right_pointers_in_each_node/solution.py
+++ my-folder/problems/populating_next_right_pointers_in_each_node/solution.py
@@ -1,24 +1,36 @@
-"""
-# Definition for a Node.
 class Node:
-    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+    def __init__(self, val=0, left=None, right=None, next=None):
         self.val = val
         self.left = left
         self.right = right
         self.next = next
-"""
 
 class Solution:
-    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
-        if not root:
-            return None
-
-        if(root.left):
-            root.left.next=root.right
-        if root.right and root.next:
-            root.right.next=root.next.left
-        self.connect(root.left)
-        self.connect(root.right)
+    def connect(self, root: 'Node') -> 'Node':
+        if not root or (not root.left and not root.right):
+            return root
         
-            
+        if root.left and root.right:
+            root.left.next = root.right
+            root.right.next = self.get_next_has_children_node(root)
+        
+        if root.left is None:
+            root.right.next = self.get_next_has_children_node(root)
+        
+        if root.right is None:
+            root.left.next = self.get_next_has_children_node(root)
+        
+        root.right = self.connect(root.right)
+        root.left = self.connect(root.left)
+        
         return root
+    
+    def get_next_has_children_node(self, root: 'Node') -> 'Node':
+        while root.next:
+            if root.next.left:
+                return root.next.left
+            if root.next.right:
+                return root.next.right
+            root = root.next
+        
+        return None
